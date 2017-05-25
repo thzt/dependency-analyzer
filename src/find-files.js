d9c661d4b8c12a0910114e159aed091edb8f2bd7
thzt <thzt.xx@gmail.com>
2017-05-25 15:52:06 +0800

    fix bug: base dir in the final graph is not absolute path

--- src/find-files.js
+++ src/find-files.js
@@ -2,7 +2,10 @@ const path = require('path');
 const recursiveReaddirSync = require('recursive-readdir-sync');
 
 const findFiles = ({ dirs, type, exclude }) =>
-    [].concat.apply([], dirs.map(dir =>
-        recursiveReaddirSync(path.resolve(dir)).filter(path => type.test(path) && !exclude.test(path))));
+    [].concat.apply(
+        [],
+        dirs.map(dir => path.resolve(dir))
+            .map(absoluteDir => recursiveReaddirSync(absoluteDir).filter(path => type.test(path) && !exclude.test(path)))
+    );
 
 module.exports = findFiles;
